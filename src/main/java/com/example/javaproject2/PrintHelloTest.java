7db04c54c06820d4b0653c0b5b9b5c505de84ed7
BuildTools <unconfigured@null.spigotmc.org>
2023-04-19 14:20:59 +0900

    23년 4월 19일 java강의 실습 업로드 -JavaIn

--- src/main/java/com/example/javaproject2/PrintHelloTest.java
+++ src/main/java/com/example/javaproject2/PrintHelloTest.java
@@ -1,6 +1,7 @@
 package com.example.javaproject2;
 
 public class PrintHelloTest {
+
     public static void main(String[] args) {
         PrintHello printHello = new PrintHello();
         printHello.printHello();
