f4a107d0c06344224a2e81e1ea14b674bbf888d4
BuildTools <unconfigured@null.spigotmc.org>
2023-04-20 17:53:29 +0900

    23년 4월 20일 코드업 문제 풀이 업로드 : 1015~1024

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1018.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1018 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] StrArr = scanner.next().split(":");
+        System.out.printf("%s:%s", StrArr[0], StrArr[1]);
+
+    }
+}
