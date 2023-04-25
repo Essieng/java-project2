2155f68928a6f7bfe0f9958b1dda092fc0e82b52
BuildTools <unconfigured@null.spigotmc.org>
2023-04-25 15:04:43 +0900

    23년 4월 25일 CodeUp 풀이 업로드: 1031~1046

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1032.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1032 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(Integer.toHexString(scanner.nextInt()));
+    }
+}
